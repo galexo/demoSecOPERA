e08e7831d461aee4c7734121f6ef3932f9b4a252
galexo <g.alexopou@gmail.com>
2024-07-30 19:18:23 +0300

    add custom_cmake and csmake

---
+++ custom_cmake/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "/mnt/esp-idf-v4.4.7/tools/tools/xtensa-esp32-elf/esp-2021r2-patch5-8.4.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/mnt/esp-idf-v4.4.7/examples/custom_bootloader/bootloader_hooks/build/tmp_dir_xtensa/ar")
+set(CMAKE_ASM_COMPILER_AR "/mnt/esp-idf-v4.4.7/examples/custom_bootloader/bootloader_hooks/build/tmp_dir_xtensa/ar")
+set(CMAKE_RANLIB "/usr/bin/ranlib")
+set(CMAKE_ASM_COMPILER_RANLIB "/mnt/esp-idf-v4.4.7/tools/tools/xtensa-esp32-elf/esp-2021r2-patch5-8.4.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc-ranlib")
+set(CMAKE_LINKER "/mnt/esp-idf-v4.4.7/examples/custom_bootloader/bootloader_hooks/build/tmp_dir_xtensa/ld")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
